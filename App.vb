b22c368b3517599c25d2837632b1fa3bd5270f09
YodeSkye <YodeSkye@outlook.com>
2025-09-19 15:02:00 -0400

    Created AppEvents file

--- App.vb
+++ App.vb
@@ -1,29 +1,6 @@
 ﻿
 Namespace My
 
-	Partial Friend Class MyApplication
-
-		'Events
-		Public Sub New()
-			MyBase.New(VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-			Me.IsSingleInstance = False
-			Me.EnableVisualStyles = True
-			Me.SaveMySettingsOnExit = False
-			Me.ShutdownStyle = VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
-		End Sub
-		Protected Overrides Function OnStartup(e As ApplicationServices.StartupEventArgs) As Boolean
-			If e.Cancel Then : Return False
-			Else
-				My.App.Initialize()
-				Return True
-			End If
-		End Function
-		Protected Overrides Sub OnCreateMainForm()
-			Me.MainForm = My.App.frmMain
-		End Sub
-
-	End Class
-
 	Friend Module App
 
 		'Declarations
---
+++ ApplicationEvents.vb
@@ -0,0 +1,27 @@
+﻿
+Namespace My
+
+    Partial Friend Class MyApplication
+
+        'App Events
+        Public Sub New()
+            MyBase.New(VisualBasic.ApplicationServices.AuthenticationMode.Windows)
+            Me.IsSingleInstance = False
+            Me.EnableVisualStyles = True
+            Me.SaveMySettingsOnExit = False
+            Me.ShutdownStyle = VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
+        End Sub
+        Protected Overrides Function OnStartup(e As ApplicationServices.StartupEventArgs) As Boolean
+            If e.Cancel Then : Return False
+            Else
+                My.App.Initialize()
+                Return True
+            End If
+        End Function
+        Protected Overrides Sub OnCreateMainForm()
+            Me.MainForm = My.App.frmMain
+        End Sub
+
+    End Class
+
+End Namespace
